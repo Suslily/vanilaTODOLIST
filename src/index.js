334d4547bdf673b914f407b8ee3a1587656165f0
Suslily <96573215+Suslily@users.noreply.github.com>
2022-06-04 00:19:43 +0900

    タスク完了機能まで実装した

--- src/index.js
+++ src/index.js
@@ -10,14 +10,30 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    deleteFromIncompleteList(div.parentNode);
+
+    // 完了リストに追加する
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    // div以下を初期化
+    addTarget.textContent = null;
+
+    const p = document.createElement("p");
+    p.innerText = inputText;
+    addTarget.appendChild(p);
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻る";
+    addTarget.appendChild(backButton);
+    // console.log(addTarget);
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // 削除ボタンを生成
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
-  completeButton.addEventListener("click", () => {
-    alert("削除");
+  deleteButton.addEventListener("click", () => {
+    deleteFromIncompleteList(div.parentNode);
   });
 
   // pを作成
@@ -37,6 +53,10 @@ const onClickAdd = () => {
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
+};
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
